1efd0c9f1d25f27b1103415ce5ff05fc821ba1fe
Duy Ngo <106200147+Duybo007@users.noreply.github.com>
2024-03-28 14:09:45 -0700

    quote

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -13,11 +13,11 @@ function Quote() {
           />
         </div>
         <div className="text-center text-3xl md:text-5xl leading-relaxed pt-16">
-          playing with action figures is not about escaping reality. it's about
-          creating a new reality where anything is possible
+          playing with action figures is not about escaping reality. it&apos;s
+          about creating a new reality where anything is possible
         </div>
         <div className="text-center text-2xl md:text-3xl pt-14">
-            j.k. rowling (brishish author, creator of the happy potter series)
+          j.k. rowling (brishish author, creator of the happy potter series)
         </div>
       </div>
     </section>
