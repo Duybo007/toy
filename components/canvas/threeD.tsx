9cfb1fa46b621b4c0c322959329904cfcd3a35da
Duy Ngo <106200147+Duybo007@users.noreply.github.com>
2024-04-04 11:22:32 -0700

    add threejs sazabi

---
+++ components/canvas/threeD.tsx
@@ -0,0 +1,80 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "./canvasLoader";
+
+const ThreeD = ({ isMobile }: any) => {
+  const sazabi = useGLTF("./3D/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={1} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={5} />
+      <primitive
+        object={sazabi.scene}
+        scale={isMobile ? 1.8 :2.2}
+        position={[0, -3, 0]}
+        rotation={[0, 1.5, 0]}
+      />
+    </mesh>
+  );
+};
+
+const ThreeDCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event: any) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense
+        fallback={<CanvasLoader />}
+      >
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <ThreeD isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ThreeDCanvas
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,28 @@ import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
 import MonthlyPromo from "@/components/MonthlyPromo";
 import Quote from "@/components/Quote";
+import StarsCanvas from "@/components/canvas/starCanvas";
+import ThreeDCanvas from "@/components/canvas/threeD";
 import { heroImageBg } from "@/constants";
 import { client, urlFor } from "@/lib/client";
 import { useEffect } from "react";
 
 export default function Home({ highlightProducts, heroImagesArray }: any) {
-  console.log(highlightProducts)
+  console.log(highlightProducts);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
     <main>
       <Hero heroImagesArray={heroImagesArray} />
-      <Highlights highlightProducts={highlightProducts}/>
+      <Highlights highlightProducts={highlightProducts} />
       <Epic />
       <MonthlyPromo />
       <Quote />
+      <div className="h-screen w-full relative">
+        <ThreeDCanvas />
+        <StarsCanvas />
+      </div>
       <Contact />
     </main>
   );
@@ -30,17 +36,17 @@ export const getServerSideProps = async () => {
     '*[_type == "product" ]{ _id, image, name, slug, price, details, categories ,"highlight": true, bought}';
   const highlightProducts = await client.fetch(query);
   highlightProducts.map((product: any) => {
-    const image = product.image[0]? urlFor(product.image[0])?.url() : ""
-    product.imageUrl = image
-  })
+    const image = product.image[0] ? urlFor(product.image[0])?.url() : "";
+    product.imageUrl = image;
+  });
 
   let heroImagesArray: any = [];
   const queryHero = '*[_type == "heroImgs" ]';
   const heroImages = await client.fetch(queryHero);
 
   heroImages.map((img: any, index: number) => {
-    const image = img.image[0]? urlFor(img.image[0])?.url() : "";
-    heroImagesArray.push({"url":image, "bg": heroImageBg[index]});
+    const image = img.image[0] ? urlFor(img.image[0])?.url() : "";
+    heroImagesArray.push({ url: image, bg: heroImageBg[index] });
   });
   return {
     props: { highlightProducts, heroImagesArray },
